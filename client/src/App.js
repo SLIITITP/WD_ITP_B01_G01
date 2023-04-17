cb224edeb9e5c2c9f8aec90b9cbfa085e079ca26
Yenuli Senevirathna <it21381768@my.sliit.lk>
2023-04-17 15:03:57 +0530

    contact

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import "./App.css"
+import "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+    <div className="container">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            
+          </Routes>
+          {/* <Footer /> */}
+        </div>
+        </BrowserRouter>
+    )
+  }
 }
-
-export default App;
