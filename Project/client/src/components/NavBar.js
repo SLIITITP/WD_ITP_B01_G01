3ab539bded2cfcc3c3f17308d2556564add991be
Yenuli Senevirathna <it21381768@my.sliit.lk>
2023-04-21 22:55:08 +0530

    ccc

--- Project/client/src/components/NavBar.js
+++ Project/client/src/components/NavBar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
                 <a class="nav-link" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/product">Spirits</a>
+                <a class="nav-link" href="/informationForm">Spirits</a>
               </li> 
               <li class="nav-item">
                 <a class="nav-link" href="#services">Wine</a>
