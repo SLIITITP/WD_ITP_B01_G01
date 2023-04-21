86d5ca215b68e93b470c32923304a1925fdf1665
Harini Senevirathne <it21386022@my.sliit.lk>
2023-04-21 16:13:15 +0530

    init

---
+++ Project/routes/posts.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const posts = require('../models/posts');
+const router = express.Router();
+
+
+//save post
+router.post('/post', (req, res) => {
+    let newPost = new posts(req.body);
+    newPost.save((err) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        return res.status(200).json({
+            success: newPost
+        });
+    });
+});
+
+
+//get post
+router.get('/posts', (req, res) => {
+    posts.find().exec((err, posts) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            existingPosts: posts
+        });
+    });
+});
+
+//update post
+
+router.put('/post/:id', (req, res) => {
+    posts.findByIdAndUpdate(   
+        req.params.id,
+        {
+            $set: req.body
+        },
+        (err, post) => {
+            if (err) {
+                return res.status(400).json({ error: err });
+            }
+            return res.status(200).json({
+                success: "Updated successfully"
+            });
+        }
+    );
+});
+
+
+//delete post
+
+router.delete('/post/:id', (req, res) => {
+    posts.findByIdAndRemove(req.params.id).exec((err, deletedPost) => {
+        if (err) return res.status(400).json({
+            message: "Delete unsuccessful", err
+        });
+        return res.json({
+            message: "Delete successful", deletedPost
+        });
+    });
+});
+
+//get specific post
+
+router.get("/post/:id", (req, res) => {
+    let postId = req.params.id;
+    posts.findById(postId , (err, post) => {
+            if (err) {
+                return res.status(400).json({ success: false, err })
+            }
+            return res.status(200).json({
+                success: true,
+                post
+            });
+        });
+});
+
+
+
+module.exports = router;
---
+++ Project/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const postRoutes = require('./routes/posts');
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(postRoutes);
+app.use(cors());
+mongoose.set('strictQuery', true);
+const port = 8000;
+const url = 'mongodb+srv://minsandi:minsandi123@mernapp.cnpzawc.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Database not connected', err));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
